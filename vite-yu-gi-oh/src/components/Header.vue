ea17ca749352bd04ac01449e04d6b0797f527ba8
Piccio <lorenzo.hl.picchi@gmail.com>
2024-06-13 23:19:20 +0200

    created an header.vue file

---
+++ vite-yu-gi-oh/src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <header>
+        <h1>Yu-Gi-Oh Card Browser</h1>
+    </header>
+</template>
+
+<style scoped>
+header {
+    background: #333;
+    color: white;
+    padding: 1rem;
+}
+</style>
