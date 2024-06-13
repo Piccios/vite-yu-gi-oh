79eee2828b623fb3f62daf377ee1c17a519a3f8a
Piccio <lorenzo.hl.picchi@gmail.com>
2024-06-14 00:12:13 +0200

    added logo

--- vite-yu-gi-oh/src/components/Header.vue
+++ vite-yu-gi-oh/src/components/Header.vue
@@ -1,13 +1,22 @@
 <template>
     <header>
-        <h1>Yu-Gi-Oh Card Browser</h1>
+        <img src="../assets/PngItem_1722617.png" alt="logo">
+        <h1>Yu-Gi-Oh Api</h1>
     </header>
 </template>
 
 <style scoped>
 header {
-    background: #333;
-    color: white;
+    background: white;
+    color: black;
     padding: 1rem;
+    text-align: start;
+    display: flex;
+    align-items: center;
+}
+
+header img{
+    height: 30px;
+    margin-right: 1rem;
 }
 </style>
