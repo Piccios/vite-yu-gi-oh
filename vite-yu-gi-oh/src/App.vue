e3dfb6df2b36e2e1d4c3424ebe20b653dbfa5e96
Piccio <lorenzo.hl.picchi@gmail.com>
2024-06-13 23:32:03 +0200

    imported header and main

--- vite-yu-gi-oh/src/App.vue
+++ vite-yu-gi-oh/src/App.vue
@@ -1,12 +1,29 @@
-<script setup>
-
-import './style.scss'
-</script>
-
 <template>
-
+  <div id="app">
+    <Header />
+    <MainContent />
+  </div>
 </template>
 
-<style lanf="scss">
+<script>
+import Header from './components/Header.vue'
+import MainContent from './components/MainContent.vue'
 
+export default {
+  components: {
+    Header,
+    MainContent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
